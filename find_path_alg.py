b30861e0a264d2f6945b8b82d9e761783180a19e
Coco Xiao <cocoxiao1998@yahoo.com>
2019-12-07 20:32:15 -0500

    Modified find_path_alg.py. Added more code to while loop. Currently going through neighbors of current node. Need to add code there. Need to think of boundaries for what pixel values are traversable. Also need to implement a function that finds the shortest path using the parent coords in node_details. Also need to decide how to display the path on the image. Path may be too small to be seen.

--- find_path_alg.py
+++ find_path_alg.py
@@ -33,13 +33,18 @@ def is_destination(img, x, y, ending_position):
 
 # calculates heuristics (diagonal)
 def heuristics(img, x, y, ending_position):
-    # getting distance
+    # getting V/H distance
+    d1 = 10 # 1 * 10
     x_end = ending_position[0]
     y_end = ending_position[1]
     x_distance = abs(x - x_end)
     y_distance = abs(y - y_end)
+
+    # getting diagonal distance
+    d2 = 14 # sqrt(2) * 10
+    diag_distance = abs(x_distance - y_distance)
     
-    h = max(x_distance, y_distance)
+    h = min(x_distance, y_distance) * d1 + diag_distance * d2
     return h
 
 # function for printing the path? Might not need. Might use for changing the pixel values
@@ -50,7 +55,7 @@ def print_path():
 # A* alg
 def find_path_alg(img, w, h, starting_position, ending_position, path_color):
     # if the user chose the starting and ending position to be the same
-    if is_destination(img, starting_position[0], ending_position[1], ending_position):
+    if is_destination(img, starting_position[0], starting_position[1], ending_position):
         print("The starting and ending position you have chosen is the same.")
         print("Exiting program.")
         exit()
@@ -59,30 +64,73 @@ def find_path_alg(img, w, h, starting_position, ending_position, path_color):
     # 5 for f, g, h values and parent x, y values
     node_details = np.full((w, h, 5), -1)
 
-    # create a list that stores all nodes that have been evaluated (part of the path now)
+    # create a 2D-list that marks all nodes that have been evaluated by 1, 0 if not
     closed_list = np.zeros((w, h))
  
-    # create an open list that will contains nodes with calculated f costs
+    # creating an open list that will contains nodes with calculated f costs
     # each index will contain: [f, [x, y]]
     open_list = []
 
     # putting the starting position in open list with its f as 0
     open_list.append([0.0, [starting_position[0], starting_position[1]]])
 
-    # value for if the destination is reached
-    # end = False
-
     while len(open_list) != 0:
-        # temp node that contains the current node
+        # set current equal to node with the lowest f-value
         if len(open_list) == 1:
             current = open_list[0]
         else:
+            current = open_list[0]
             for node in range(open_list):
+                # using <= since last appended nodes are the most recent and usually closer to end
+                if node[0] <= current[0]:
+                    current = node
                 exit()
-                print("hi")
 
+        # vars for current (x, y) for easier readability
+        cur_x = current[1][0] 
+        cur_y = current[1][1]
 
+        # removing current node from open list
+        open_list.remove(current)
 
-    # returning
-    print("Path has finished being calculated")
+        # appending (x, y) to closed list
+        closed_list[cur_x][cur_y] = 1
+
+        # checking if current node is the destination
+        if is_destination(img, cur_x, cur_y, ending_position):
+            print("Path has finished being calculated")
+            return
+
+        # getting the neighbor nodes
+        else:
+            neighbor_list_VH = [(cur_x - 1, cur_y), (cur_x + 1, cur_y), (cur_x, cur_y + 1), (cur_x, cur_y - 1)]
+            neighbor_list_diag = (cur_x - 1, cur_y + 1), (cur_x - 1, cur_y - 1), (cur_x + 1, cur_y + 1), (cur_x + 1, cur_y - 1)
+                
+            for neighbor in neighbor_list_VH:
+                # vars for neighbor (x, y) for readability
+                n_x = neighbor[0]
+                n_y = neighbor[1]
+
+                # check that neighbor exists, is traversable and is marked as 0 in closed list
+                if (exists(img, n_x, n_y, w, h) and 
+                    is_traversable(img, n_x, n_y) and 
+                    closed_list[n_x][n_y] == 0):
+                    # checking if current node is starting position in order to get right value g
+                    if current[1] == starting_position:
+                        g = 10
+                    else: 
+                        g = node_details[cur_x][cur_y][1] + 10
+                    # getting other values
+                    h = heuristics(img, n_x, n_y, ending_position)
+                    f = g + h
+
+                    # checking if f-cost to neighbor is shorter or if neighbor not in open list
+                    
+                    # storing details in node details
+                    node_details[n_x][n_y][0] = f
+                    node_details[n_x][n_y][1] = g
+                    node_details[n_x][n_y][2] = h
+                    node_details[n_x][n_y][3] = cur_x
+                    node_details[n_x][n_y][4] = cur_y
+
 
